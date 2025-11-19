4a249f3f173520bc572dffd33036398f80e50f05
ckcurry <159097633+ckcurry@users.noreply.github.com>
2025-11-18 18:19:22 -0800

    Create HouseholdHome.tsx

---
+++ src/pages/HouseholdHome.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { client } from "../client";
+import type { Schema } from "../../amplify/data/resource";
+
+export function HouseholdHome() {
+  // <-- paste your HouseholdHome component code here
+}
