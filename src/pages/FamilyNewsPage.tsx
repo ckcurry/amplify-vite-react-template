7d6ed27f26a2c307a4c14c51809b65e0868c937b
ckcurry <159097633+ckcurry@users.noreply.github.com>
2025-11-18 23:30:57 -0800

    Update FamilyNewsPage.tsx removing link

--- src/pages/FamilyNewsPage.tsx
+++ src/pages/FamilyNewsPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { client } from "../client";
 import type { Schema } from "../../amplify/data/resource";
 
