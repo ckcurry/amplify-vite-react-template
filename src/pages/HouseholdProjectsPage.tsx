4ae2f1150fa6378bde25a8339a2a0663ba510a79
ckcurry <159097633+ckcurry@users.noreply.github.com>
2025-11-18 21:25:06 -0800

    Create HouseholdProjectsPage.tsx

---
+++ src/pages/HouseholdProjectsPage.tsx
@@ -0,0 +1,5 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { client } from "../client";
+import type { Schema } from "../../amplify/data/resource";
+
