5c3d0175dd7fbe5af145142ae07660a9ffd0c6f9
ckcurry <159097633+ckcurry@users.noreply.github.com>
2025-11-18 19:20:01 -0800

    Create Dashboard.tsx

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import { uploadData, getUrl } from "aws-amplify/storage";
+import { client } from "../client";
+import type { Schema } from "../../amplify/data/resource";
+
+export function Dashboard() {
+  // <-- paste your Dashboard code here exactly as it was
+}
